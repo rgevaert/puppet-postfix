e2a4aed296a3798b00efea6a141c6525a5585f8b
Rudy Gevaert <rudy.gevaert+github@ugent.be>
2012-03-22 13:27:22 +0100

    add extra packages for postfix on debian

--- manifests/install.pp
+++ manifests/install.pp
@@ -15,6 +15,42 @@ class postfix::install
     }
   }
 
+  if($postfix::params::postfix_mysql_package != '')
+  {
+    package {
+      $postfix::params::postfix_mysql_package:
+        ensure   => installed,
+        provider => $postfix::params::postfix_package_provider;
+    }
+  }
+
+  if($postfix::params::postfix_cdb_package != '')
+  {
+    package {
+      $postfix::params::postfix_cdb_package:
+        ensure   => installed,
+        provider => $postfix::params::postfix_package_provider;
+    }
+  }
+
+  if($postfix::params::postfix_pcre_package != '')
+  {
+    package {
+      $postfix::params::postfix_pcre_package:
+        ensure   => installed,
+        provider => $postfix::params::postfix_package_provider;
+    }
+  }
+
+  if($postfix::params::postfix_pgsql_package != '')
+  {
+    package {
+      $postfix::params::postfix_pgsql_package:
+        ensure   => installed,
+        provider => $postfix::params::postfix_package_provider;
+    }
+  }
+
   if($postfix::params::mailx_package != '')
   {
     package {
